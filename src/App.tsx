bf56684eaec37deaf6789ecc0e6d3b2d052789f3
kelter-js <spawnkillerqw@gmail.com>
2025-02-25 16:16:23 +0500

    feat: app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,41 @@
+import { CardsField } from "./components/CardsField";
+import { Modal } from "./components/Modal";
+import { Statistics } from "./components/Statistics";
+import { useGameState } from "./hooks/useGameState";
+import { MainContainer, FillerImage } from "./styled.index";
+
 const App = () => {
-  return <div></div>;
+  const {
+    handlePickCard,
+    isGameOver,
+    amountOfTurns,
+    cardsField,
+    handleRestartGame,
+  } = useGameState();
+
+  return (
+    <MainContainer>
+      {isGameOver && (
+        <Modal onClose={handleRestartGame}>
+          <Statistics score={amountOfTurns} />
+        </Modal>
+      )}
+
+      <FillerImage
+        alt="filler"
+        src={`${process.env.PUBLIC_URL}/assets/scribble.svg`}
+        width="100%"
+        height="100%"
+      />
+
+      <CardsField
+        cardsList={cardsField}
+        onPickCard={handlePickCard}
+        isGameOver={isGameOver}
+        key={String(isGameOver)}
+      />
+    </MainContainer>
+  );
 };
 
 export default App;
